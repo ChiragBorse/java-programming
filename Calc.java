dc36827245b10b33564f6b41986b11065b656fac
ChiragBorse <123314804+ChiragBorse@users.noreply.github.com>
2023-04-04 10:39:53 +0530

    Add files via upload

---
+++ Calc.java
@@ -0,0 +1,12 @@
+public class Calc
+{
+
+  public static void main(string [] args)
+  {
+int num1=2;
+int num2=3,num3;
+num3 = num1+num2;
+System.out.println("The sum  of two numbers is : " + num3);
+
+}
+}
